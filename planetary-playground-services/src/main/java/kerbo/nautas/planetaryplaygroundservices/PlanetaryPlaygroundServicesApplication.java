c22b242e115be0b60c78c0c51802bd6cf13d8d9b
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-10-19 00:09:04 +0200

    Added spring boot starter project for the services

---
+++ planetary-playground-services/src/main/java/kerbo/nautas/planetaryplaygroundservices/PlanetaryPlaygroundServicesApplication.java
@@ -0,0 +1,13 @@
+package kerbo.nautas.planetaryplaygroundservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetaryPlaygroundServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetaryPlaygroundServicesApplication.class, args);
+	}
+
+}
---
+++ planetary-playground-services/src/test/java/kerbo/nautas/planetaryplaygroundservices/PlanetaryPlaygroundServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package kerbo.nautas.planetaryplaygroundservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetaryPlaygroundServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
