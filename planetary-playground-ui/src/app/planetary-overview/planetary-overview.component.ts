8e23f181b458fc116420a05ba0c587dd82215c69
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-10-19 17:45:53 +0200

    Retrieved results from service

--- planetary-playground-ui/src/app/planetary-overview/planetary-overview.component.ts
+++ planetary-playground-ui/src/app/planetary-overview/planetary-overview.component.ts
@@ -1,4 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { MatRadioChange } from '@angular/material/radio';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+export interface PredefResponse {
+  ArrayPlanets: Planet[];
+  star: StarObject;
+}
+
+export interface Planet {
+  haveAtmosphere: boolean,
+  isSolid: boolean,
+  mass: number,
+  name: string,
+  numberOfSatellites: number,
+  pressure: number,
+  radius: number,
+  temperature: number
+}
+
+export interface StarObject {
+  constanteLuminosidadSol: number,
+  constanteRadioSol: number,
+  constanteSigma: number,
+  luminosidad: number,
+  magnitudAbsoluta: number,
+  masa: number,
+  radio: number
+  secuencia: number,
+  temperatura: number,
+  tipoEspectral: string
+}
 
 @Component({
   selector: 'app-planetary-overview',
@@ -6,12 +37,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planetary-overview.component.scss']
 })
 export class PlanetaryOverviewComponent implements OnInit {
+  urlBase: string = 'http://planetplayground-env.wuakashtt6.eu-west-2.elasticbeanstalk.com';
 
-  constructor() {
+  constructor(private _http: HttpClient) {
 
   }
 
   ngOnInit() {
   }
 
+  starChange(_$event: MatRadioChange) {
+    if (_$event.value !== "CUSTOM") {
+      this.requestCustom(_$event.value);
+    }
+  }
+
+  requestCustom(starName: string) {    
+    const requestedStar = { "SOLAR": starName };
+
+    this._http.post(this.urlBase + '/star', requestedStar, {
+      headers:
+        { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+    }).subscribe((response: PredefResponse) => {
+      //Represent things
+    });
+  }
 }
