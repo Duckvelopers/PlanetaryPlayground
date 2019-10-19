c9eee7eaf8961aea21b43fa002273b07178830c8
Juan Antonio Lagos Carrera <lagoscarrera@gmail.com>
2019-10-19 21:16:55 +0200

    idioma ingles

---
+++ planetary-playground-services/src/main/java/kerbonautas/planetaryplaygroundservices/EsStarDefs.java
@@ -0,0 +1,12 @@
+package kerbonautas.planetaryplaygroundservices;
+
+public class EsStarDefs {
+
+	public static String explicacionesSecuencia() {
+		StringBuilder str = new StringBuilder();
+		
+		str.append("");
+		
+		return str.toString();
+	}
+}
