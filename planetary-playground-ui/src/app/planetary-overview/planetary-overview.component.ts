d97af09538787c98141a43f88b284272c78eb92e
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-10-19 12:38:34 +0200

    Styled everything and spiced it a bit, updated usage of css files to be scss files

--- planetary-playground-ui/src/app/planetary-overview/planetary-overview.component.ts
+++ planetary-playground-ui/src/app/planetary-overview/planetary-overview.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-planetary-overview',
   templateUrl: './planetary-overview.component.html',
-  styleUrls: ['./planetary-overview.component.css']
+  styleUrls: ['./planetary-overview.component.scss']
 })
 export class PlanetaryOverviewComponent implements OnInit {
 
   constructor() {
 
-   }
+  }
 
   ngOnInit() {
   }
