9d9b742920e17d0778af7e23450c1d6150b03603
Kelfindel <Kelfindel@DESKTOP-GIOEVQA>
2019-10-19 13:50:31 +0200

    created

---
+++ planetary-playground-services/src/main/java/kerbonautas/planetaryplaygroundservices/StarVariables.java
@@ -0,0 +1,20 @@
+package kerbonautas.planetaryplaygroundservices;
+
+public class StarVariables {
+	public static class Secuencia{
+		public static int SUPERGIGANTE		= 1;
+		public static int GIGANTE			= 3;
+		public static int SECUENCIAP		= 5;
+		public static int ENANABLANCA		= 7;
+	}
+	public static class TipoEspectral{
+		public static String O		= "O";
+		public static String B		= "B";
+		public static String A		= "A";
+		public static String F		= "F";
+		public static String G		= "G";
+		public static String K		= "K";
+		public static String M		= "M";
+		public static String T		= "T";
+	}
+}
