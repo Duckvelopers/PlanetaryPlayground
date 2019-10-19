57af877978a6f55deef0f6d2a5ba121555b15b29
cgmar <cgmar@DESKTOP-81CT0QU>
2019-10-19 14:07:37 +0200

    nueva clase nombrePlanetasSistemaSolar en StarVariables

--- planetary-playground-services/src/main/java/kerbonautas/planetaryplaygroundservices/StarVariables.java
+++ planetary-playground-services/src/main/java/kerbonautas/planetaryplaygroundservices/StarVariables.java
@@ -1,12 +1,14 @@
 package kerbonautas.planetaryplaygroundservices;
 
 public class StarVariables {
+	
 	public static class Secuencia{
 		public static int SUPERGIGANTE		= 1;
 		public static int GIGANTE			= 3;
 		public static int SECUENCIAP		= 5;
 		public static int ENANABLANCA		= 7;
 	}
+	
 	public static class TipoEspectral{
 		public static String O		= "O";
 		public static String B		= "B";
@@ -20,5 +22,16 @@ public class StarVariables {
 	}
 	public static double masaPlaneta (double masa, int exponente) {
 		return Math.pow(masa, exponente);
+	}
+	
+	public static class nombrePlanetasSistemaSolar{
+		public static String VENUS = "Venus";
+		public static String MERCURIO = "Mercurio";
+		public static String TIERRA = "Tierra";
+		public static String MARTE = "Marte";
+		public static String JUPITER = "Jupiter";
+		public static String SATURNO = "Saturno";
+		public static String URANO = "Urano";
+		public static String NEPTUNO = "Neptuno";
 	}
 }
