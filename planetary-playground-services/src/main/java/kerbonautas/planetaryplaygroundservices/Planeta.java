166b67b75d198f18333ab456c80c00f7ecc43dd9
Kelfindel <Kelfindel@DESKTOP-GIOEVQA>
2019-10-19 13:41:35 +0200

    deleted Id and changed temp to double

--- planetary-playground-services/src/main/java/kerbonautas/planetaryplaygroundservices/Planeta.java
+++ planetary-playground-services/src/main/java/kerbonautas/planetaryplaygroundservices/Planeta.java
@@ -2,9 +2,8 @@ package kerbonautas.planetaryplaygroundservices;
 
 public class Planeta {
 
-	private int ID;
 	private int radious;
-	private int temperature;
+	private double temperature;
 	private int numberOfSatellites;
 	private boolean isSolid;
 	private int mass;
@@ -12,8 +11,7 @@ public class Planeta {
 	private String name;
 	private double pressure;
 	
-	public Planeta(int ID, int rad, int temp, int sat, boolean solid, int mass, boolean haveAtmosphere, String name, double pressure) {
-		setID(ID);
+	public Planeta(int rad, double temp, int sat, boolean solid, int mass, boolean haveAtmosphere, String name, double pressure) {
 		setRadious(rad);
 		setTemperature(temp);
 		setNumberOfSatellites(sat);
@@ -25,21 +23,19 @@ public class Planeta {
 	}
 	
 	public void setRadious(int i) {radious = i;}
-	public void setTemperature(int i) {temperature = i;}
+	public void setTemperature(double i) {temperature = i;}
 	public void setNumberOfSatellites(int i) {numberOfSatellites = i;}
 	public void setIsSolid(boolean i) {isSolid = i;}
 	public void setMass(int i) {mass = i;}
-	public void setID(int i) {ID = i;}
 	public void setAtmosphere(boolean i) {haveAtmosphere = i;}
 	public void setName(String i) {name = i;}
 	public void setPressure(double i) {pressure = i;}
 	
 	public int getRadious() {return radious;}
-	public int getTemperature() {return temperature;}
+	public double getTemperature() {return temperature;}
 	public int getNumberOfSatellites() {return numberOfSatellites;}
 	public boolean getIsSolid() {return isSolid;}
 	public int getMass() {return mass;}
-	public int getID() {return ID;}
 	public boolean getAtmosphere() {return haveAtmosphere;}
 	public String getName() {return name;}
 	public double getPressure() {return pressure;}
