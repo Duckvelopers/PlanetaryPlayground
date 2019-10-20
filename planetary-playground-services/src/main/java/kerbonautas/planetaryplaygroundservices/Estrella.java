b8c1cfe35053ff002b0fe019850cf8c2a1bd38ba
cgmar <cgmar@DESKTOP-81CT0QU>
2019-10-20 10:10:16 +0200

    Cambiado el calculo de la masa y el radio

--- planetary-playground-services/src/main/java/kerbonautas/planetaryplaygroundservices/Estrella.java
+++ planetary-playground-services/src/main/java/kerbonautas/planetaryplaygroundservices/Estrella.java
@@ -124,16 +124,19 @@ public class Estrella {
 	
 	public double calcularMasa(double auxRadio, double auxTemperatura) {
 		double auxMasa = 0;
+		double auxMasaSolar = 1.989 * Math.pow(10, 30);
 		auxMasa = 4 * Math.PI * constanteSigma * Math.pow(constanteRadioSol, 2) * Math.pow(auxRadio, 2) * Math.pow(auxTemperatura, 4);
-		auxMasa = auxMasa / constanteLuminosidadSol;
 		auxMasa = Math.pow(auxMasa, 2/7);
+		auxMasa = auxMasa / auxMasaSolar;
 		return auxMasa;
 	}
 	
 	public double calcularRadio(double auxMasa, double auxTemperatura) {
 		double auxRadio = 0;
-		auxRadio = constanteLuminosidadSol * Math.pow(auxMasa, 7/2);
+		double auxMasaSolar = 1.989 * Math.pow(10, 30);
+		auxRadio = Math.pow(auxMasa, 7/2) * Math.pow(auxMasaSolar, 7/2);
 		auxRadio = auxRadio / (4 * Math.PI * constanteSigma * Math.pow(constanteRadioSol, 2) * Math.pow(auxTemperatura, 4));
+		auxRadio = Math.pow(auxRadio, 1/2);
 		return auxRadio;
 	}
 	
